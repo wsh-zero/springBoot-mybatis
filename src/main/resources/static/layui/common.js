308a5cca734fd5c12118d3da4f72e2681964a6df
zero <254353372>
2019-01-01 19:17:37 +0800

    调整

--- src/main/resources/static/layui/common.js
+++ src/main/resources/static/layui/common.js
@@ -7,3 +7,15 @@
 function remove_last_char(data) {
     return data.substring(0, data.length - 1);
 }
+
+/**
+ * 去掉对象中value为空的属性
+ * @param object
+ */
+function removeProperty(object) {
+    for (var prop in object) {
+        if (object[prop] === '') {
+            delete object[prop]
+        }
+    }
+}
