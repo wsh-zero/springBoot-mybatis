8f86dc751de45ca5715ea5ac8c290dc0fa918ac5
wsh <254353372@11.com>
2019-03-25 14:40:47 +0800

    只用传内容,图片文件由前端处理

--- src/main/java/com/wsh/util/mail/JavaMailUtil.java
+++ src/main/java/com/wsh/util/mail/JavaMailUtil.java
@@ -2,13 +2,13 @@ package com.wsh.util.mail;
 
 import com.wsh.util.ResultUtil;
 
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.Properties;
 
@@ -81,55 +81,9 @@ public class JavaMailUtil {
         //4.设置邮件主题
         msg.setSubject(mailBean.getSubject(), "UTF-8");
 
-        //下面是设置邮件正文
-        //msg.setContent("简单的纯文本邮件！", "text/html;charset=UTF-8");
-
-        // 5. 创建图片"节点"
-        MimeBodyPart image = new MimeBodyPart();
-        // 读取本地文件
-        DataHandler dh = new DataHandler(new FileDataSource("src\\om1.jpg"));
-        // 将图片数据添加到"节点"
-        image.setDataHandler(dh);
-        // 为"节点"设置一个唯一编号（在文本"节点"将引用该ID）
-        image.setContentID("mailTestPic");
-
-        // 6. 创建文本"节点"
-        MimeBodyPart text = new MimeBodyPart();
-        // 这里添加图片的方式是将整个图片包含到邮件内容中, 实际上也可以以 http 链接的形式添加网络图片
-        text.setContent(mailBean.getContent() + "<br/><img src='cid:mailTestPic'/>", "text/html;charset=UTF-8");
-
-        // 7. （文本+图片）设置 文本 和 图片"节点"的关系（将 文本 和 图片"节点"合成一个混合"节点"）
-        MimeMultipart mm_text_image = new MimeMultipart();
-        mm_text_image.addBodyPart(text);
-        mm_text_image.addBodyPart(image);
-        mm_text_image.setSubType("related");    // 关联关系
-
-        // 8. 将 文本+图片 的混合"节点"封装成一个普通"节点"
-        // 最终添加到邮件的 Content 是由多个 BodyPart 组成的 Multipart, 所以我们需要的是 BodyPart,
-        // 上面的 mailTestPic 并非 BodyPart, 所有要把 mm_text_image 封装成一个 BodyPart
-        MimeBodyPart text_image = new MimeBodyPart();
-        text_image.setContent(mm_text_image);
-
-        // 9. 创建附件"节点"
-        MimeBodyPart attachment = new MimeBodyPart();
-        // 读取本地文件
-        DataHandler dh2 = new DataHandler(new FileDataSource("src\\22.docx"));
-        // 将附件数据添加到"节点"
-        attachment.setDataHandler(dh2);
-        // 设置附件的文件名（需要编码）
-        attachment.setFileName(MimeUtility.encodeText(dh2.getName()));
-
-        // 10. 设置（文本+图片）和 附件 的关系（合成一个大的混合"节点" / Multipart ）
-        MimeMultipart mm = new MimeMultipart();
-        mm.addBodyPart(text_image);
-        mm.addBodyPart(attachment);     // 如果有多个附件，可以创建多个多次添加
-        mm.setSubType("mixed");         // 混合关系
-
-        // 11. 设置整个邮件的关系（将最终的混合"节点"作为邮件的内容添加到邮件对象）
-        msg.setContent(mm);
-        //设置邮件的发送时间,默认立即发送
+        //设置邮件正文
+        msg.setContent(mailBean.getContent(), "text/html;charset=UTF-8");
         msg.setSentDate(new Date());
-
         return msg;
     }
 }
